0e0288a9cea597e3696412fe51aa9560b141be76
Deva <dkothand@gmail.com>
2019-05-09 14:47:56 -0400

    implemented create route, curl test adds item to database

--- notes_api/controllers/notes.js
+++ notes_api/controllers/notes.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const notes = express.Router()
+const Note = require('../models/notes')
 
 notes.get('/', (req, res) => {
     res.send('Hello from the notes index!')
+})
+
+notes.post('/', (req, res) => {
+    Note.create(req.body, (err, createdNote) => {
+        if (err) {
+            res.status(400).json({error: err.message})
+        }
+        console.log(createdNote)
+        res.status(200).send(createdNote)
+    })
 })
 
 module.exports = notes
--- notes_api/server.js
+++ notes_api/server.js
@@ -8,14 +8,26 @@ const app = express()
 const PORT = 3000;
 const MONGODB_URI = 'mongodb://localhost:27017'
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+//DATABASE
+// Error / Disconnection
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+mongoose.connect('mongodb://localhost:27017/notes', { useNewUrlParser: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
 })
 
+
 // MIDDLEWARE
+app.use(express.json()) // uses json over urlencoded
 const notesController = require('./controllers/notes')
 app.use('/notes', notesController)
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 // THIS IS A COMMENT I MADE
 
 app.listen(PORT, () => {
