eb01ac6e8a7d437b5f64f7464d2b4aec409f9755
Deva <dkothand@gmail.com>
2019-05-09 14:11:38 -0400

    added middleware to enable notes controller

--- notes_api/server.js
+++ notes_api/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// MIDDLEWARE
+const notesController = require('./controllers/notes')
+app.use('/notes', notesController)
+
 app.listen(PORT, () => {
     console.log('connected on port', PORT)
 })
